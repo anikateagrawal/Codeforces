10137518ef598ace911df318f699e9f2f735f616
anikateagrawal <anikateag7316@gmail.com>
2023-03-05 11:58:34 +0530

    commit

---
+++ src/easy_800/Domino_piling.java
@@ -0,0 +1,12 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Domino_piling {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(m*n/2);
+    }
+}
