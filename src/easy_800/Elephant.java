c413743915474da6e25019802c9205c8ba31960a
anikateagrawal <anikateag7316@gmail.com>
2023-03-16 08:40:16 +0530

    commit

---
+++ src/easy_800/Elephant.java
@@ -0,0 +1,11 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Elephant {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println((int)Math.ceil(1.0*n/5));
+    }
+}
