38e727c47a9455f486593f9bb6f62eba4c4e5bc9
anikateagrawal <anikateag7316@gmail.com>
2023-03-18 10:57:12 +0530

    commit

---
+++ src/easy_800/Wrong_Subtraction.java
@@ -0,0 +1,18 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Wrong_Subtraction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        while (k-->0){
+            if(n%10==0){
+                n/=10;
+            }
+            else n--;
+        }
+        System.out.println(n);
+    }
+}
