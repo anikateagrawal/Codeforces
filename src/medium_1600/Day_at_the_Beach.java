9d8423e9e81d2ea0b09ee3dab90f9990df95a5b7
anikateagrawal <anikateag7316@gmail.com>
2023-07-13 11:17:08 +0530

    commit

---
+++ src/medium_1600/Day_at_the_Beach.java
@@ -0,0 +1,27 @@
+package medium_1600;
+
+import java.util.Scanner;
+
+public class Day_at_the_Beach {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)a[i]=sc.nextInt();
+        int lmax[]=new int[n+1];
+        int rmin[]=new int[n+1];
+        lmax[0]=Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            lmax[i+1]=Math.max(lmax[i],a[i]);
+        }
+        rmin[n]=Integer.MAX_VALUE;
+        for (int i = n-1; i >=0; i--) {
+            rmin[i]=Math.min(rmin[i+1],a[i]);
+        }
+        int p=0;
+        for (int i = 0; i < n; i++) {
+            if(lmax[i]<=rmin[i])p++;
+        }
+        System.out.println(p);
+    }
+}
