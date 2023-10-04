0ed1dd457c1114316fb545da6a9a10998ba7985c
anikateagrawal <anikateag7316@gmail.com>
2023-10-04 12:57:12 +0530

    commit

---
+++ src/easy_800/EhAb_AnD_gCd.java
@@ -0,0 +1,14 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class EhAb_AnD_gCd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int x=sc.nextInt();
+            System.out.println(1+" "+(x-1));
+        }
+    }
+}
