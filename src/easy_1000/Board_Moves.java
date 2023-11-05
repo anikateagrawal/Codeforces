a5410b2aafe7fbf905881c0184399c4e10e4ff29
anikateagrawal <anikateag7316@gmail.com>
2023-11-05 11:22:03 +0530

    commit

---
+++ src/easy_1000/Board_Moves.java
@@ -0,0 +1,19 @@
+package easy_1000;
+
+import java.util.Scanner;
+
+public class Board_Moves {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        long m[]=new long[1000000];
+        m[1]=0;
+        for(int i=1;i<m.length;i++){
+            m[i]=8L*i*i+m[i-1];
+        }
+        while (t-->0){
+            int n=sc.nextInt();
+            System.out.println(m[n/2]);
+        }
+    }
+}
