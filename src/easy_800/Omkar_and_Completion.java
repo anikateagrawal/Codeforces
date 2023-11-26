ce35d4c86ddf00f499117444858cd621d7460ae0
anikateagrawal <anikateag7316@gmail.com>
2023-11-26 09:51:04 +0530

    commit

---
+++ src/easy_800/Omkar_and_Completion.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Omkar_and_Completion {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            for(int i=0;i<n;i++) System.out.print(1+" ");
+            System.out.println();
+        }
+    }
+}
