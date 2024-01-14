a2b526bf8dc8a883c7e47cec1ff2fd647d48a99d
anikateagrawal <anikateag7316@gmail.com>
2024-01-14 10:53:59 +0530

    commit

---
+++ src/easy_800/ABC.java
@@ -0,0 +1,20 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class ABC {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            String s=sc.next();
+            if(n<3){
+                if(s.equals("11")||s.equals("00")) System.out.println("NO");
+                else
+                System.out.println("YES");
+            }
+            else System.out.println("NO");
+        }
+    }
+}
