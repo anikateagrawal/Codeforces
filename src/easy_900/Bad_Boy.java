2616ed964f1b791361175d71962f8e07ffecddf3
Anikate Agrawal <anikateag7316@gmail.com>
2024-04-19 07:19:46 +0530

    commit

---
+++ src/easy_900/Bad_Boy.java
@@ -0,0 +1,17 @@
+package easy_900;
+
+import java.util.Scanner;
+
+public class Bad_Boy {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int i=sc.nextInt();
+            int j=sc.nextInt();
+            System.out.println(1+" "+1+" "+n+" "+m);
+        }
+    }
+}
