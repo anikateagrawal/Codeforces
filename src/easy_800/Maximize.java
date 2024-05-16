291ae01906ff08eb8f0462bfb0b768901f3024e2
Anikate Agrawal <anikateag7316@gmail.com>
2024-05-16 07:22:39 +0530

    commit

---
+++ src/easy_800/Maximize.java
@@ -0,0 +1,14 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Maximize {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int x=sc.nextInt();
+            System.out.println(x-1);
+        }
+    }
+}
