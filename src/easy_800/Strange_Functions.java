21f371f5efc4440b86a0360b21f7d43939c2616b
Anikate Agrawal <anikateag7316@gmail.com>
2024-05-31 09:35:34 +0530

    commit

---
+++ src/easy_800/Strange_Functions.java
@@ -0,0 +1,14 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Strange_Functions {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            String s=sc.next();
+            System.out.println(s.length());
+        }
+    }
+}
