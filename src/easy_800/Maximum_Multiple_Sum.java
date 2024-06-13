d932ef17aa7da95555727b60d966e30b55f5e09e
Anikate Agrawal <anikateag7316@gmail.com>
2024-06-13 09:10:03 +0530

    commit

---
+++ src/easy_800/Maximum_Multiple_Sum.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Maximum_Multiple_Sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            if(n==3) System.out.println(3);
+            else System.out.println(2);
+        }
+    }
+}
