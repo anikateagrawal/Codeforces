63e9106993d98188590d2130c2dcdb58be2a07de
Anikate Agrawal <anikateag7316@gmail.com>
2024-08-08 11:52:47 +0530

    commit

--- src/easy_800/Legs.java
+++ src/easy_800/Legs.java
@@ -2,7 +2,7 @@ package easy_800;
 
 import java.util.Scanner;
 
-public class Legs {
+public class Legs  {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
