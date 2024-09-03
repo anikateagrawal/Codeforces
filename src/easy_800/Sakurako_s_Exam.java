11ea13094ffd47964714558055b1b304f00439e3
anikateagrawal <anikateag7316@gmail.com>
2024-09-03 14:13:20 +0530

    commit

---
+++ src/easy_800/Sakurako_s_Exam.java
@@ -0,0 +1,20 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Sakurako_s_Exam {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int ones=sc.nextInt();
+            int twos=sc.nextInt();
+            if(ones%2==0 && twos%2==0) System.out.println("YES");
+            else if(ones%2==1) System.out.println("NO");
+            else{
+                if(ones>0) System.out.println("YES");
+                else System.out.println("NO");
+            }
+        }
+    }
+}
