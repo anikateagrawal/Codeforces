0f4b05105377032272c8dcb1eef6d0469665d57b
anikateagrawal <anikateag7316@gmail.com>
2024-09-04 13:49:57 +0530

    commit

---
+++ src/easy_800/Minimize.java
@@ -0,0 +1,16 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Minimize {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(b-a);
+        }
+
+    }
+}
