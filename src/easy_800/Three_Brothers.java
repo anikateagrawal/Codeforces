d4df43be2fd4de5c8238fcb0dd10fd77214a6e37
anikateagrawal <anikateag7316@gmail.com>
2024-09-12 13:37:21 +0530

    commit

---
+++ src/easy_800/Three_Brothers.java
@@ -0,0 +1,11 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Three_Brothers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int b=sc.nextInt()+sc.nextInt();
+        System.out.println(6-b);
+    }
+}
