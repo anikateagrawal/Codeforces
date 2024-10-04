6ce27ef5ff9e06faed5efc9f1f993a31d4aaac33
anikateagrawal <anikateag7316@gmail.com>
2024-10-04 04:17:32 -0400

    commit

---
+++ src/easy_800/Array_Divisibility.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Array_Divisibility {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            for (int i=1;i<=n;i++) System.out.print(i+" ");
+            System.out.println();
+        }
+    }
+}
