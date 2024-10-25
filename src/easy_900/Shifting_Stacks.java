a4676680a2100bcf2e098484fe4244c370c6085a
anikateagrawal <anikateag7316@gmail.com>
2024-10-25 04:33:47 -0400

    commit

--- src/easy_900/Shifting_Stacks.java
+++ src/easy_900/Shifting_Stacks.java
@@ -1,6 +1,8 @@
 package easy_900;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Shifting_Stacks {
     public static void main(String[] args) {
@@ -8,10 +10,21 @@ public class Shifting_Stacks {
         int t=sc.nextInt();
         while (t-->0){
             int n=sc.nextInt();
-            long sum=0;
-            for (int i=0;i<n;i++)sum+=sc.nextInt();
-            long min=n*(n-1)/2;
-            if(sum>=min) System.out.println("YES");
+            long a[]=new long[n];
+            for (int i=0;i<n;i++) {
+                a[i] = sc.nextInt();
+            }
+            boolean flag=true;
+            for (int i=0;i<n;i++){
+                if(a[i]<i){
+                    flag=false;
+                    break;
+                }
+                if(i<n-1){
+                    a[i+1]+=a[i]-i;
+                }
+            }
+            if(flag) System.out.println("YES");
             else System.out.println("NO");
         }
     }
