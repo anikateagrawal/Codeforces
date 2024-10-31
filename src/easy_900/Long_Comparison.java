5b5dfa8f3dd89e62303f4abe7ba8ad213ed786c7
anikateagrawal <anikateag7316@gmail.com>
2024-10-31 09:04:33 -0400

    commit

--- src/easy_900/Long_Comparison.java
+++ src/easy_900/Long_Comparison.java
@@ -9,14 +9,27 @@ public class Long_Comparison {
         while (t-->0){
             String s1=sc.next();
             int z1=sc.nextInt();
-            while (z1-->0)s1+="0";
             String s2=sc.next();
             int z2=sc.nextInt();
-            while (z2-->0)s2+="0";
-            int c=s1.length()==s2.length()?s1.compareTo(s2):s1.length()-s2.length();
-            if(c>0) System.out.println(">");
-            else if(c==0) System.out.println("=");
-            else System.out.println("<");
+            int l1=s1.length()+z1;
+            int l2=s2.length()+z2;
+            if(l1>l2){
+                System.out.println(">");
+            }
+            else if(l2>l1){
+                System.out.println("<");
+            }
+            else{
+                while (s1.length()!=s2.length()){
+                    if(s1.length()<s2.length())s1+="0";
+                    else s2+="0";
+                }
+                int c=s1.compareTo(s2);
+                if(c==0) System.out.println("=");
+                else if (c>0) System.out.println(">");
+                else System.out.println("<");
+            }
+
         }
     }
 }
