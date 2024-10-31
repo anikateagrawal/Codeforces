ba0760baf73c7a60b56a72e6eee7430216645d84
anikateagrawal <anikateag7316@gmail.com>
2024-10-31 07:20:12 -0400

    commit

---
+++ src/easy_900/Long_Comparison.java
@@ -0,0 +1,22 @@
+package easy_900;
+
+import java.util.Scanner;
+
+public class Long_Comparison {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            String s1=sc.next();
+            int z1=sc.nextInt();
+            while (z1-->0)s1+="0";
+            String s2=sc.next();
+            int z2=sc.nextInt();
+            while (z2-->0)s2+="0";
+            int c=s1.compareTo(s2);
+            if(c>0) System.out.println(">");
+            else if(c==0) System.out.println("=");
+            else System.out.println("<");
+        }
+    }
+}
