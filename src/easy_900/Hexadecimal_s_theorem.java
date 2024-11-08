379306f57e8bc743d183bbe2b94b01f156939d32
anikateagrawal <anikateag7316@gmail.com>
2024-11-08 02:00:38 -0500

    commit

---
+++ src/easy_900/Hexadecimal_s_theorem.java
@@ -0,0 +1,11 @@
+package easy_900;
+
+import java.util.Scanner;
+
+public class Hexadecimal_s_theorem {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(0+" "+0+" "+n);
+    }
+}
