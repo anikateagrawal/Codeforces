157b3b1ec16bc950bc4e1870d2007a0c5ad2dfa5
anikateagrawal <anikateag7316@gmail.com>
2024-11-10 23:39:40 -0500

    commit

---
+++ src/easy_800/Rewards.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Rewards {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt()+sc.nextInt()+sc.nextInt();
+        int m=sc.nextInt()+sc.nextInt()+sc.nextInt();
+        int n=sc.nextInt();
+        int r=(c+4)/5+(m+9)/10;
+        if(r<=n) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
--- src/other/abc.java
+++ src/other/abc.java
@@ -1,6 +1,6 @@
 package other;
-class abc{
-    public static void main(String[] args) {
-        System.out.printf("hi");
-    }
+import java.util.Scanner;
+
+public class abc {
+    
 }
