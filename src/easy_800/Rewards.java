f70522f09d2bbf1e7e0f6c21f40c38ab94d295a7
anikateagrawal <anikateag7316@gmail.com>
2024-11-11 01:13:44 -0500

    commit

--- src/easy_800/Rewards.java
+++ src/easy_800/Rewards.java
@@ -8,6 +8,7 @@ public class Rewards {
         int c=sc.nextInt()+sc.nextInt()+sc.nextInt();
         int m=sc.nextInt()+sc.nextInt()+sc.nextInt();
         int n=sc.nextInt();
+
         int r=(c+4)/5+(m+9)/10;
         if(r<=n) System.out.println("YES");
         else System.out.println("NO");
