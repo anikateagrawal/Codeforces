bbaf4a3b7dba3c6faec1a35a91e3703f116b1b62
anikateagrawal <anikateag7316@gmail.com>
2024-11-11 01:18:57 -0500

    commit

---
+++ src/easy_800/Suborrays.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Suborrays {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            for (int i=1;i<=n;i++) System.out.println(i+" ");
+            System.out.println();
+        }
+    }
+}
