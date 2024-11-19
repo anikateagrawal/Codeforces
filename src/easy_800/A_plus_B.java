3ac456b396bd5d3372fa65761c1ab1d23bb5264d
anikateagrawal <anikateag7316@gmail.com>
2024-11-19 00:32:28 -0500

    commit

---
+++ src/easy_800/A_plus_B.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class A_plus_B {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
