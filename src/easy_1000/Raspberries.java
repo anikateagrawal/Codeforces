501ea43c4e7f56f33b500804df84c9fa014c8f0e
anikateagrawal <anikateag7316@gmail.com>
2024-12-09 11:19:31 +0530

    commit

---
+++ src/easy_1000/Raspberries.java
@@ -0,0 +1,30 @@
+package easy_1000;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Raspberries {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int e=0;
+            int ans=Integer.MAX_VALUE;
+            for (int i=0;i<n;i++){
+                int a=sc.nextInt();
+                int c=a%k==0?0:k-(a%k);
+                ans=Math.min(ans,c);
+                if(a%2==0)e++;
+            }
+            if(k==4) {
+                if(e>=2) System.out.println(0);
+                else
+                System.out.println(Math.min(ans,2));
+            }
+            else System.out.println(ans);
+        }
+    }
+}
