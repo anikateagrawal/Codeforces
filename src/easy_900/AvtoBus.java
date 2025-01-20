7ac1f65ef9071adcb629434796c5a4e0d30f6f9b
anikateagrawal <anikateag7316@gmail.com>
2025-01-20 11:19:14 +0530

    commit

---
+++ src/easy_900/AvtoBus.java
@@ -0,0 +1,22 @@
+package easy_900;
+
+import java.util.Scanner;
+
+public class AvtoBus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            long n=sc.nextLong();
+            long a=n/12;
+            long f=n%12;
+            long b=0;
+            if(f==6||f==4)b=1;
+            else if(f==8||f==10)b=2;
+            else if(f!=0){
+                System.out.println(-1);continue;
+            }
+            System.out.println(a*2+b+" "+(a*3+b));
+        }
+    }
+}
