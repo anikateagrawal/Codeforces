8959406a382ea159a6ffe2de75c517763ba5f390
anikateagrawal <anikateag7316@gmail.com>
2025-01-20 11:30:50 +0530

    commit

--- src/easy_900/AvtoBus.java
+++ src/easy_900/AvtoBus.java
@@ -13,7 +13,10 @@ public class AvtoBus {
             long b=0;
             if(f==6||f==4)b=1;
             else if(f==8||f==10)b=2;
-            else if(f!=0){
+            else if(f==2 && n>12) {
+                System.out.println(a*2+1+" "+(a*3));continue;
+            }
+            else if(f!=0 ){
                 System.out.println(-1);continue;
             }
             System.out.println(a*2+b+" "+(a*3+b));
