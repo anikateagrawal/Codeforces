cac6ac2cdff6228cac844b0890ac1ebd1cb7a002
anikateagrawal <anikateag7316@gmail.com>
2025-02-16 08:21:22 +0530

    commit

---
+++ src/easy_1200/Plus_Minus_Permutation.java
@@ -0,0 +1,27 @@
+package easy_1200;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Plus_Minus_Permutation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int lcm=(x*y)/gcd(x,y);
+            int ln=n/lcm;
+            long xn=n-(n/x-ln);
+            long yn=n/y-ln;
+//            System.out.println(xn+" "+yn+" "+lcm+" "+ln);
+            long ans=1L*n*(n+1)/2-xn*(xn+1)/2 - yn*(yn+1)/2;
+            System.out.println(ans);
+        }
+    }
+    static int gcd(int a,int b){
+        if(b==0)return a;
+        return gcd(b,a%b);
+    }
+}
