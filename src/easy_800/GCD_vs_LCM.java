d1a51b3041f1d934d726a1c6d505be58ea290581
anikateagrawal <anikateag7316@gmail.com>
2025-03-02 13:09:44 +0530

    commit

---
+++ src/easy_800/GCD_vs_LCM.java
@@ -0,0 +1,14 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class GCD_vs_LCM {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            System.out.println(n-3+" 1 1 1");
+        }
+    }
+}
