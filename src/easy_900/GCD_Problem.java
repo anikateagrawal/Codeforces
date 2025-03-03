00ba256f187cd361d1238a04a61869261ab0d0e4
anikateagrawal <anikateag7316@gmail.com>
2025-03-03 12:53:39 +0530

    commit

--- src/easy_900/GCD_Problem.java
+++ src/easy_900/GCD_Problem.java
@@ -8,7 +8,14 @@ public class GCD_Problem {
         int t=sc.nextInt();
         while (t-->0){
             int n=sc.nextInt();
-            System.out.println(1+" "+(n-2)+" "+1);
+            if(n%2==0){
+                System.out.println((n/2)+" "+(n/2-1)+" "+1);
+            }
+            else {
+                int d=n/2;
+                if(d%2==0) System.out.println((d-1)+" "+(d+1)+" "+1);
+                else System.out.println((d-2)+" "+(d+2)+" "+1);
+            }
         }
     }
 }
