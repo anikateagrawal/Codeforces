f234a726b4fec7ae0bdfdc9df1971342bbd032f1
anikateagrawal <anikateag7316@gmail.com>
2025-03-03 12:25:57 +0530

    commit

---
+++ src/easy_900/GCD_Problem.java
@@ -0,0 +1,14 @@
+package easy_900;
+
+import java.util.Scanner;
+
+public class GCD_Problem {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            System.out.println(1+" "+(n-2)+" "+1);
+        }
+    }
+}
