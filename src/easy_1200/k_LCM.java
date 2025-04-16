beef5e8f0a5dedf9516f9ab8785fde9a701cdc86
anikateagrawal <anikateag7316@gmail.com>
2025-04-16 14:26:09 +0530

    commit

--- src/easy_1200/k_LCM.java
+++ src/easy_1200/k_LCM.java
@@ -9,7 +9,10 @@ public class k_LCM {
         while (t-->0){
             int n=sc.nextInt();
             int k=sc.nextInt();
-            if(n%2==0){
+            if(n%4==0){
+                System.out.println((n/2)+" "+(n/4)+" "+(n/4));
+            }
+            else if(n%2==0){
                 n-=2;
                 System.out.println(2+" "+(n/2)+" "+(n/2));
             }
