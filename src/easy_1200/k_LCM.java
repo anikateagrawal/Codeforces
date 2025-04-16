60183adc02bf0e7c8d3c108050fcc6596a1b1d07
anikateagrawal <anikateag7316@gmail.com>
2025-04-16 14:00:07 +0530

    commit

---
+++ src/easy_1200/k_LCM.java
@@ -0,0 +1,19 @@
+package easy_1200;
+
+import java.util.Scanner;
+
+public class k_LCM {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            if(n%2==0){
+                n-=2;
+                System.out.println(2+" "+(n/2)+" "+(n/2));
+            }
+            else System.out.println(1+" "+(n/2)+" "+(n/2));
+        }
+    }
+}
