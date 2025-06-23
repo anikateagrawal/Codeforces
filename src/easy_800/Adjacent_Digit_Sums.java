8321416b2c5fff6a39c53b46d9c34722e16866a9
anikateagrawal <anikateag7316@gmail.com>
2025-06-23 09:22:09 +0530

    commit

---
+++ src/easy_800/Adjacent_Digit_Sums.java
@@ -0,0 +1,15 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Adjacent_Digit_Sums {
+    public static void main(String[]a) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t-- > 0) {
+            int x = s.nextInt(), y = s.nextInt();
+            long d = (long) x - y + 1;
+            System.out.println(d >= 0 && d % 9 == 0 ? "Yes" : "No");
+        }
+    }
+}
