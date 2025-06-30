96d6c22405e3c986551bc3e41cbc76931795cbb7
anikateagrawal <anikateag7316@gmail.com>
2025-06-30 10:31:48 +0530

    commit

---
+++ src/easy_800/Shrink.java
@@ -0,0 +1,19 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Shrink {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int t=sc.nextInt();
+
+        while(t-->0){
+            int n=sc.nextInt();
+
+            for(int i=2;i<=n;i++)System.out.print(i+" ");
+
+            System.out.println(1);
+        }
+    }
+}
