8bf2e3f90a9e3aa0b196e6eaaf56f636d2d02b7b
anikateagrawal <anikateag7316@gmail.com>
2025-09-04 17:37:57 +0530

    commit

---
+++ src/easy_800/Three_Decks.java
@@ -0,0 +1,13 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Three_Decks {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println((a+b+c)%3==0 && b<=((a+b+c)/3)?"Yes":"No");
+    }
+}
