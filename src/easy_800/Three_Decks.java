370c1f45390fef0bbb52fcc0d0445e70ca115e78
anikateagrawal <anikateag7316@gmail.com>
2025-09-04 17:38:51 +0530

    commit

--- src/easy_800/Three_Decks.java
+++ src/easy_800/Three_Decks.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Three_Decks {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        System.out.println((a+b+c)%3==0 && b<=((a+b+c)/3)?"Yes":"No");
+        int t=sc.nextInt();
+        while (t-->0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println((a + b + c) % 3 == 0 && b <= ((a + b + c) / 3) ? "Yes" : "No");
+        }
     }
 }
