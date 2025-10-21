9093a911a24703974cb37265315c1aefe51eb8a7
anikateagrawal <anikateag7316@gmail.com>
2025-10-21 12:56:30 +0530

    commit

--- src/easy_800/Difference_Operations.java
+++ src/easy_800/Difference_Operations.java
@@ -11,7 +11,7 @@ public class Difference_Operations {
             int a[]=new int[n];
             for (int i=0;i<n;i++)a[i]=sc.nextInt();
             boolean flag=true;
-            for (int i=2;i<n;i++){
+            for (int i=1;i<n;i++){
                 if(a[i]%a[0]!=0)flag=false;
             }
             if (flag) System.out.println("YES");
