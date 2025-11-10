61df88afb8c49947166339c681468b37daa7c99a
anikateagrawal <anikateag7316@gmail.com>
2025-11-10 10:15:36 +0530

    commit

---
+++ src/easy_800/El_fucho.java
@@ -0,0 +1,14 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class El_fucho {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            System.out.println(n+n-2);
+        }
+    }
+}
