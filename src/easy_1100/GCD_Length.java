781a93dae3d7aba1e9c80e519ef9fe5544194d19
anikateagrawal <anikateag7316@gmail.com>
2025-11-25 09:43:50 +0530

    commit

--- src/easy_1100/GCD_Length.java
+++ src/easy_1100/GCD_Length.java
@@ -1,34 +1,18 @@
 package easy_1100;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class GCD_Length {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while (t-->0){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            int c=sc.nextInt();
-            int z=(int)Math.pow(10,c-1);
-            z=nextPrime(z);
-            int x= (int) Math.ceil(Math.pow(10,a-1)/z);
-            int y=(int)Math.ceil(Math.pow(10,b-1)/z);
-            x=nextPrime(x);
-            y=nextPrime(y);
-            if(x==y)y=nextPrime(y+1);
-            System.out.println((z*x)+" "+(z*y));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+            int x = (int) Math.pow(10, a - 1), y = (int) Math.pow(10, b - 1), gcd = (int) Math.pow(10, c - 1);
+            x = x - (x % gcd);
+            y = y - (y % gcd) + gcd;
+            System.out.println(x + " " + y);
         }
-    }
-    static int nextPrime(int n){
-        while (!isPrime(n))n++;
-        return n;
-    }
-    static boolean isPrime(int n){
-        if(n<=1)return false;
-        for (int i=2;i*i<=n;i++){
-            if(n%i==0)return false;
-        }
-        return true;
     }
 }
