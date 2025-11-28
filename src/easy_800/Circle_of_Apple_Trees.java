14f83a5c6f839d4106ba3f55d012e8576310dbf6
anikateagrawal <anikateag7316@gmail.com>
2025-11-28 08:49:32 +0530

    commit

---
+++ src/easy_800/Circle_of_Apple_Trees.java
@@ -0,0 +1,18 @@
+package easy_800;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Circle_of_Apple_Trees {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            Set<Integer> s=new HashSet<>();
+            for (int i=0;i<n;i++)s.add(sc.nextInt());
+            System.out.println(s.size());
+        }
+    }
+}
