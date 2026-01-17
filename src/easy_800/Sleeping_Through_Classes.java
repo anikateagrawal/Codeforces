723f611d418e9d875294e2189051f3c03875413b
anikateagrawal <anikateag7316@gmail.com>
2026-01-17 08:53:32 +0530

    commit

--- src/easy_800/Sleeping_Through_Classes.java
+++ src/easy_800/Sleeping_Through_Classes.java
@@ -11,13 +11,18 @@ public class Sleeping_Through_Classes {
             int k=sc.nextInt();
             String s=sc.next();
             int z=0;
-            int i=0;
-            while (i<s.length()){
-                char ch=s.charAt(i++);
-                if(ch=='1')i+=k;
-                else z++;
+            int ans=0;
+            for (int i=0;i<s.length();i++){
+                char ch=s.charAt(i);
+                if(ch=='1'){
+                    z=-1*k;
+                }
+                else {
+                    z++;
+                    if(z>0)ans++;
+                }
             }
-            System.out.println(z);
+            System.out.println(ans);
         }
     }
 }
