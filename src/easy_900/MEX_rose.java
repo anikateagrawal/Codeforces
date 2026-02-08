9af173431f09c830e4f68c38ac7b1328d75d744e
anikateagrawal <anikateag7316@gmail.com>
2026-02-08 12:23:11 +0530

    commit

---
+++ src/easy_900/MEX_rose.java
@@ -0,0 +1,24 @@
+package easy_900;
+
+import java.util.Scanner;
+
+public class MEX_rose {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int a[]=new int[n+1];
+            for (int i=0;i<n;i++){
+                a[sc.nextInt()]++;
+            }
+            int o=0;
+            for (int i=0;i<k;i++){
+                if(a[i]==0)o++;
+            }
+            o=a[k];
+            System.out.println(o);
+        }
+    }
+}
