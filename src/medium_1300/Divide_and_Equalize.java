69af9a24dd8b9186a83b7cc31b3eee32436445cb
anikateagrawal <anikateag7316@gmail.com>
2026-02-11 17:08:22 +0530

    commit

---
+++ src/medium_1300/Divide_and_Equalize.java
@@ -0,0 +1,37 @@
+package medium_1300;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Divide_and_Equalize {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            for(int i=0;i<n;i++)a[i]=sc.nextInt();
+            Map<Integer,Integer> map=new HashMap<>();
+            for (int i=0;i<n;i++){
+                for (int p=2;;p++){
+                    while (a[i]%p==0){
+                        map.put(p,map.getOrDefault(p,0)+1);
+                        a[i]/=p;
+                    }
+                    if(a[i]==1)break;
+                }
+            }
+            boolean flag=true;
+            for (int f:map.keySet()){
+                if(map.get(f)%n!=0){
+                    flag=false;
+                    break;
+                }
+            }
+//            System.out.println(map);
+            if (flag) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
