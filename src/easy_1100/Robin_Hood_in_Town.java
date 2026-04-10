a0816c662f8b4f3d3ee91009625ad20c66f86fce
anikateagrawal <anikateag7316@gmail.com>
2026-04-10 08:28:29 +0530

    commit

---
+++ src/easy_1100/Robin_Hood_in_Town.java
@@ -0,0 +1,38 @@
+package easy_1100;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Robin_Hood_in_Town {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            long sum=0;
+            for (int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+                sum+=a[i];
+            }
+            if (n<=2) {
+                System.out.println(-1);
+                continue;
+            }
+            Arrays.sort(a);
+            int ravg=a[n/2];
+            int lo=0,hi=Integer.MAX_VALUE;
+            int x=hi;
+            while (lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                double avg=(sum+mid+0.0)/n;
+                if (avg/2>ravg){
+                    x=mid;
+                    hi=mid-1;
+                }
+                else lo=mid+1;
+            }
+            System.out.println(x);
+        }
+    }
+}
--- src/other/abc.java
+++ src/other/abc.java
@@ -1,39 +1,29 @@
 package other;
 
-
 import java.util.*;
 
 public class abc {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
-        while (t-->0){
+        while(t-->0){
             int n=sc.nextInt();
-            String ans="1";
-            List<Integer> l=new ArrayList<>();
-            l.add(sc.nextInt());
-            boolean flag=false;
-            for (int i=1;i<n;i++){
-                int v=sc.nextInt();
-                if(!flag){
-                    if(v>=l.get(l.size()-1)){
-                        l.add(v);
-                        ans+="1";
+            int m=sc.nextInt();
+            Set<Integer> rows=new HashSet<>();
+            Set<Integer> cols=new HashSet<>();
+            for (int i=0;i<n;i++){
+                for (int j=0;j<m;j++){
+                    int a=sc.nextInt();
+                    if(a==1){
+                        rows.add(i);
+                        cols.add(j);
                     }
-                    else if(v<=l.get(0)){
-                        l.add(v);
-                        ans+="1";
-                        flag=true;
-                    }
-                    else ans+="0";
                 }
-                else if(v>=l.get(l.size()-1) && v<=l.get(0)){
-                    l.add(v);
-                    ans+="1";
-                }
-                else ans+="0";
             }
-            System.out.println(ans);
+            int ur=n-rows.size();
+            int uc=m-cols.size();
+            int turns=Math.min(ur,uc);
+            System.out.println(turns%2==1?"Ashish":"Vivek");
         }
     }
 }
