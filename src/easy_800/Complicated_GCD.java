7d611abfd5a4afbc0bff04aadda9834bc72c90cd
anikateagrawal <anikateag7316@gmail.com>
2026-06-15 08:13:08 +0530

    commit

---
+++ src/easy_800/Complicated_GCD.java
@@ -0,0 +1,13 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Complicated_GCD {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        if (a.equals(b)) System.out.println(a);
+        else System.out.println(1);
+    }
+}
