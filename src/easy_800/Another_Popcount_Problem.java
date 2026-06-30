7f41ad8ee48d940914e6a069558ff2a1c1437a6c
anikateagrawal <anikateag7316@gmail.com>
2026-06-30 09:10:42 +0530

    commit

---
+++ src/easy_800/Another_Popcount_Problem.java
@@ -0,0 +1,24 @@
+package easy_800;
+
+import java.util.Scanner;
+
+public class Another_Popcount_Problem {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            if(k>=n) System.out.println(n);
+            else {
+                int d=n/k;
+                String s=Integer.toBinaryString(d);
+                int m=s.length()-1;
+                int n1=(1<<m)-1;
+                int r = n - n1*k;
+                int ans=m*k + r/(1<<(m));
+                System.out.println(ans);
+            }
+        }
+    }
+}
