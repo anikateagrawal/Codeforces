17cfa859ebb9967716b278ce79b2123aa2b6d709
anikateagrawal <anikateag7316@gmail.com>
2026-07-10 12:20:04 +0530

    commit

---
+++ src/easy_1200/Next_Test.java
@@ -0,0 +1,18 @@
+package easy_1200;
+
+import java.util.Scanner;
+
+public class Next_Test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        boolean a[]=new boolean[n+2];
+        for (int i=0;i<n;i++)a[i]=true;
+        for (int i=1;i<=n+1;i++){
+            if (!a[i]){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
