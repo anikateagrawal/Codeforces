c3f267abb6d5ddf75c9aa3edddee0bd9ec03395b
anikateagrawal <anikateag7316@gmail.com>
2026-07-31 08:11:01 +0530

    commit

---
+++ src/easy_800/Sequence_Game.java
@@ -0,0 +1,20 @@
+package easy_800;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sequence_Game {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            for (int i=0;i<n;i++)a[i]=sc.nextInt();
+            int x=sc.nextInt();
+            Arrays.sort(a);
+            if (x>=a[0] && x<=a[n-1]) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
