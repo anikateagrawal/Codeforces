f3e0b024259d3a9039989892933b3a3e546b299c
anikateagrawal <anikateag7316@gmail.com>
2026-08-11 08:22:31 +0530

    commit

---
+++ src/easy_800/Greedy_Monocarp.java
@@ -0,0 +1,26 @@
+package easy_800;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Greedy_Monocarp {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int a[]=new int[n];
+            for (int i=0;i<n;i++)a[i]=sc.nextInt();
+            int sum=0;
+            Arrays.sort(a);
+            for (int i=n-1;i>=0;i--){
+                if (sum+a[i]>k){
+                    break;
+                }
+                sum+=a[i];
+            }
+            System.out.println(k-sum);
+        }
+    }
+}
